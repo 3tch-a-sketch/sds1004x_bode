392e4e2690bbfc4bb8e86cdac9348ecbb681235b
Matt Wachowski <matt.wachowski@gmail.com>
2020-12-20 15:55:42 -0800

    Add a new driver for FYGen.
    I copied files from:
      https://github.com/mattwach/fygen
    
    then created a wrapper to allow them to be used.
    
    This should, in theory, give better bode plot performance
    because fygen is a closed-loop system (i.e. instead
    of sleeping for 0.5 seconds, it implements reads and
    retries which will generally allow for < 0.5 second
    update times)
    
    All of this still needs to be tested and benchmarked.  Not
    yet ready for use.

---
+++ sds1004x_bode/awgdrivers/fy.py
@@ -0,0 +1,88 @@
+"""API wrapper for cpnverting BaseAWG->fygen."""
+
+from base_awg import BaseAWG
+import fygen
+
+AWG_ID = "fy"
+AWG_OUTPUT_IMPEDANCE = 50.0
+
+class Error(Exception):
+    pass
+
+class InvalidChannelError(Error):
+    pass
+
+def _map_channel(channel):
+    if channel not in (1,2):
+        raise InvalidChannelError('Invalid channel: %d' % channel)
+    return channel - 1
+
+class FygenAWG(BaseAWG):
+    '''
+      Use fygen libraries (For feeltech devices.)
+    '''
+    
+    SHORT_NAME = "fy"
+
+    def __init__(self, port, baud_rate=None, timeout=None):
+        self.fy = None
+        self.port = port
+        self.load_impedance = {
+                1: 1000000.0,
+                2: 1000000.0,
+        }
+
+    def connect(self):
+        if not self.fy:
+            self.fy = fygen.FYGen(port=self.port)
+    
+    def disconnect(self):
+        if self.fy:
+            self.fy.close()
+            self.fy = None
+    
+    def initialize(self):
+        self.connect()
+        self.enable_output(1, False)
+        self.enable_output(2, False)
+    
+    def get_id(self):
+        return AWG_ID
+    
+    def enable_output(self, channel, on):
+        self.fy.set(_map_channel(channel), enable=on)
+    
+    def set_frequency(self, channel, freq):
+        fpart, ipart = math.modf(freq)
+        self.fy.set(
+                _map_channel(channel),
+                freq_hz=int(ipart),
+                freq_uhz=int(fpart * 1000000))
+        
+    def set_phase(self, phase):
+        self.fy.set(
+                1,  # always channel 2
+                phase_degrees=phase)
+
+    def set_wave_type(self, channel, wvtp):
+        del wvtp  # This parameter is ignored
+        self.fy.set(_map_channel(channel), wave='sin')
+    
+    def set_amplitue(self, channel, amp):
+        self.fy.set(
+            _map_channel(channel),
+            volts=round(
+                self.load_impedance[channel] / 
+                (AWG_OUTPUT_IMPEDANCE + self.load_impedance), 2))
+    
+    def set_offset(self, channel, offset):
+        pass
+    
+    def set_load_impedance(self, channel, z):
+        if z > 10000000.0:
+            # We can only set voltage within one hundredth of a volt and
+            # it's rounded.
+            z = 10000000.0
+        _map_channel(channel)  # only used for the check
+        self.load_impedance[channel] = z
+    
