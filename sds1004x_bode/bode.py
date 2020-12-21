5d10f8d29b9bbe64b29f16eaaae1bb190d2c34f4
Matt Wachowski <matt.wachowski@gmail.com>
2020-12-20 21:32:27 -0800

    Added optional --rpcbind_port and --vxi11_port flags.
    This makes it possible to create NAT entries (via iptables)
    and avoid running bode.py as root
    
    e.g.
    
    sudo iptables -t nat -A PREROUTING -p tcp --dport 111 -j REDIRECT --to-port 8000
    python bode.py --rpcbind_port 8000 --vxi11_port 8001

--- sds1004x_bode/bode.py
+++ sds1004x_bode/bode.py
@@ -4,45 +4,66 @@ Created on May 5, 2018
 @author: dima
 '''
 
+import argparse
 import sys
 from awg_server import AwgServer
 from awg_factory import awg_factory
 
-DEFAULT_AWG = "dummy"
-DEFAULT_PORT = "/dev/ttyUSB0"
-DEFAULT_BAUD_RATE = None
+PARSER = argparse.ArgumentParser(
+    description=('Emulates a Siglent AWG well enough to support Bode Plots '
+        'with a Siglent Oscilliscope and non-Siglent AWG\'s'))
+
+PARSER.add_argument(
+    'awg_name',
+    help='The name of the AWG driver to use',
+    nargs='?',
+    default='dummy')
+
+PARSER.add_argument(
+    'awg_port',
+    help='The AWG port (or device path) to connect to',
+    nargs='?',
+    default='/dev/ttyUSB0')
+
+PARSER.add_argument(
+    'awg_baud_rate',
+    help='The baud rate to use',
+    nargs='?',
+    default='')
+
+PARSER.add_argument(
+    '--rpcbind_port',
+    help='The RPC bind port to listen on',
+    type=int,
+    default=111)
+
+PARSER.add_argument(
+    '--vxi11_port',
+    help='The VXI11 port to listen on',
+    type=int,
+    default=703)
+
+ARGS = PARSER.parse_args()
+
 
 if __name__ == '__main__':
-    # Extract AWG name from parameters
-    if len(sys.argv) >= 2:
-        awg_name = sys.argv[1]
-    else:
-        awg_name = DEFAULT_AWG
-        
-    # Extract port name from parameters
-    if len(sys.argv) >= 3:
-        awg_port = sys.argv[2]
-    else:
-        awg_port = DEFAULT_PORT
-        
-    # Extract AWG port baud rate from parameters
-    if len(sys.argv) == 4:
-        awg_baud_rate = int(sys.argv[3])
-    else:
-        awg_baud_rate = DEFAULT_BAUD_RATE  
-    
-    # Initialize AWG
     print "Initializing AWG..."
-    print "AWG: %s" % awg_name
-    print "Port: %s" % awg_port
-    awg_class = awg_factory.get_class_by_name(awg_name)
-    awg = awg_class(awg_port, awg_baud_rate)
+    print '  Driver: %s' % ARGS.awg_name
+    print '  Port: %s' % ARGS.awg_port
+    if ARGS.awg_baud_rate:
+        print '  Baud Rate: %s' % ARGS.awg_baud_rate
+
+    awg_class = awg_factory.get_class_by_name(ARGS.awg_name)
+    awg = awg_class(
+            ARGS.awg_port, ARGS.awg_baud_rate if ARGS.awg_baud_rate else None)
     awg.initialize()
-    
-    # Run AWG server
+
     server = None
     try:
-        server = AwgServer(awg)
+        server = AwgServer(
+                awg,
+                rpcbind_port=ARGS.rpcbind_port,
+                vxi11_port=ARGS.vxi11_port)
         server.start()
     
     except KeyboardInterrupt:
